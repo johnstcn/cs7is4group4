e05810df08c72a93b49c30c12a3fed73d0a76f4c
Cian Johnston <public@cianjohnston.ie>
2020-03-09 22:08:55 +0000

    refactor munger of tweets

--- twittermunger/main.go
+++ twittermunger/main.go
@@ -20,25 +20,25 @@ import (
 
 // User is a user
 type User struct {
-	ID int64 `json:"id"`
+	ID         int64  `json:"id"`
 	ScreenName string `json:"screen_name"`
-	Location string `json:"location"`
+	Location   string `json:"location"`
 }
 
 // Geo is a geographical thing
 type Geo struct {
-	Type string `json:"type"`
+	Type        string    `json:"type"`
 	Coordinates []float64 `json:"coordinates"`
 }
 
 // Place is a place
 type Place struct {
-	ID string `json:"id"`
-	Type string `json:"place_type"`
-	Name string `json:"name"`
-	FullName string `json:"full_name"`
+	ID          string `json:"id"`
+	Type        string `json:"place_type"`
+	Name        string `json:"name"`
+	FullName    string `json:"full_name"`
 	CountryCode string `json:"country_code"`
-	Country string `json:"country"`
+	Country     string `json:"country"`
 }
 
 // ExtendedTweet has longer text
@@ -48,56 +48,38 @@ type ExtendedTweet struct {
 
 // Tweet is a tweet with a User and maybe a Geo or a Place
 type Tweet struct {
-	ID int64 `json:"id"`
-	CreatedAt string `json:"created_at"`
-	Text string `json:"text`
-	User User `json:"user"`
-	Geo *Geo `json:"geo"`
-	Place *Place `json:"place"`
+	ID            int64          `json:"id"`
+	CreatedAt     string         `json:"created_at"`
+	Text          string         `json:"text`
+	User          User           `json:"user"`
+	Geo           *Geo           `json:"geo"`
+	Place         *Place         `json:"place"`
 	ExtendedTweet *ExtendedTweet `json:"extended_tweet"`
 }
 
-// TweetMunger munges tweets
-type TweetMunger struct {
-	CountryCode string
-	SearchExpr *regexp.Regexp
+// MungeOpts holds options related to tweet munging
+type MungeOpts struct {
+	CountryCode      string
+	SearchExpr       *regexp.Regexp
 	UserLocationExpr *regexp.Regexp
-	Writer *csv.Writer
 }
 
-func (tm *TweetMunger) processFile(fname string) {
-	f, err := os.Open(fname)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "ERR: open %s: %s", fname, err)
-		return
-	}
-	defer f.Close()
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		t, err := tm.processLine(scanner.Text())
-		if err != nil {
-			continue
-		}
-		tm.toCSV(t)
-	}
-}
-
-func (tm *TweetMunger) processLine(line string) (*Tweet, error) {
+func processLine(line string, opts MungeOpts) (*Tweet, error) {
 	var t Tweet
 	d := json.NewDecoder(strings.NewReader(line))
 	if err := d.Decode(&t); err != nil {
 		return nil, errors.Wrapf(err, "process line %q", line)
 	}
 
-	if !tm.SearchExpr.MatchString(t.Text) {
+	if !opts.SearchExpr.MatchString(t.Text) {
 		return nil, errors.New("no text match")
 	}
 
-	if !tm.UserLocationExpr.MatchString(t.User.Location) {
+	if !opts.UserLocationExpr.MatchString(t.User.Location) {
 		return nil, errors.New("no user location match")
 	}
 
-	if tm.CountryCode == "" {
+	if opts.CountryCode == "" {
 		return &t, nil
 	}
 
@@ -106,14 +88,14 @@ func (tm *TweetMunger) processLine(line string) (*Tweet, error) {
 		return nil, errors.New("no place")
 	}
 
-	if t.Place.CountryCode != tm.CountryCode {
+	if t.Place.CountryCode != opts.CountryCode {
 		return nil, errors.New("no country code match")
 	}
 
 	return &t, nil
 }
 
-func (tm *TweetMunger) toCSV(t *Tweet) {
+func toCSV(w *csv.Writer, t *Tweet) {
 	var countryCode, placeName, tweetText string
 	if t.Place != nil {
 		countryCode = t.Place.CountryCode
@@ -134,8 +116,8 @@ func (tm *TweetMunger) toCSV(t *Tweet) {
 		countryCode,
 		tweetText,
 	}
-	tm.Writer.Write(fields)
-	tm.Writer.Flush()
+	w.Write(fields)
+	w.Flush()
 }
 
 func mustParseDateToTimestamp(s string) int64 {
@@ -147,6 +129,55 @@ func mustParseDateToTimestamp(s string) int64 {
 	return t.Unix()
 }
 
+func slurpTarFile(fileName string, writer *csv.Writer, opts MungeOpts) error {
+	tarFile, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer tarFile.Close()
+
+	var b bytes.Buffer
+	tr := tar.NewReader(tarFile)
+	for {
+		b.Reset()
+		hdr, err := tr.Next()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+
+		if !strings.HasSuffix(hdr.Name, ".json.bz2") {
+			continue
+		}
+
+		_, err = io.Copy(&b, tr)
+		if err != nil {
+			return err
+		}
+
+		bzipReader := bzip2.NewReader(&b)
+		s := bufio.NewScanner(bzipReader)
+
+		for s.Scan() {
+			line := s.Text()
+			if len(line) == 0 {
+				continue
+			}
+			tweet, err := processLine(line, opts)
+			if err != nil {
+				continue
+			}
+			if tweet.ID == 0 {
+				continue
+			}
+			toCSV(writer, tweet)
+		}
+	}
+	return nil
+}
+
 func main() {
 	var countryCode string
 	var searchExpr string
@@ -155,13 +186,15 @@ func main() {
 	flag.StringVar(&userLocationExpr, "userlocationexpr", ".*", "user location expression")
 	flag.StringVar(&searchExpr, "searchexpr", ".*", "search expression")
 	flag.Parse()
-	tm := &TweetMunger{
-		CountryCode: strings.ToUpper(countryCode),
-		SearchExpr: regexp.MustCompile(searchExpr),
+	opts := MungeOpts{
+		CountryCode:      strings.ToUpper(countryCode),
+		SearchExpr:       regexp.MustCompile(searchExpr),
 		UserLocationExpr: regexp.MustCompile(userLocationExpr),
-		Writer: csv.NewWriter(os.Stdout),
 	}
-	tm.Writer.Write([]string{
+
+	writer := csv.NewWriter(os.Stdout)
+
+	writer.Write([]string{
 		"tweet_id",
 		"tweet_created_at_ts",
 		"user_screen_name",
@@ -176,51 +209,10 @@ func main() {
 		return
 	}
 
-	tarFile, err := os.Open(args[0])
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer tarFile.Close()
-
-	var b bytes.Buffer
-	tr := tar.NewReader(tarFile)
-	for {
-		b.Reset()
-		hdr, err := tr.Next()
-		if err == io.EOF {
-			break;
-		} else if err != nil {
-			fmt.Println(err)
+	for _, arg := range args {
+		if err := slurpTarFile(arg, writer, opts); err != nil {
+			os.Stderr.WriteString(err.Error())
 			os.Exit(1)
-		}
-
-		if !strings.HasSuffix(hdr.Name, ".json.bz2") {
-			continue
-		}
-
-		_, err = io.Copy(&b, tr)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		bzipReader := bzip2.NewReader(&b)
-		s := bufio.NewScanner(bzipReader)
-		
-		for s.Scan() {
-			line := s.Text()
-			if len(line) == 0 {
-				continue
-			}
-			tweet, err := tm.processLine(line)
-			if err != nil {
-				continue
-			}
-			if tweet.ID == 0 {
-				continue
-			}
-			tm.toCSV(tweet)
 		}
 	}
 }
